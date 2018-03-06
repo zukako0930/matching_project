06e05c581b77de0f48a222e60866d26b0694d63b
橋爪友莉子 <yuyu1.run@icloud.com>
2018-03-06 15:59:23 +0900

    メッセージの投稿

--- matching/app/controllers/messages_controller.rb
+++ matching/app/controllers/messages_controller.rb
@@ -4,10 +4,22 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
+    if flash[:send_id]
+      send_id = flash[:send_id]
+      receive_id = flash[:receive_id]
+    else
+      send_id = params[:send_id]
+      receive_id = params[:receive_id]
+    end
+
     if params[:send_id]
       @messages = Message.talk(params[:receive_id], params[:send_id])
       @send_user = User.find(params[:send_id])
       @receive_user = User.find(params[:receive_id])
+    elsif send_id
+      @messages = Message.talk(receive_id, send_id)
+      @send_user = User.find(send_id)
+      @receive_user = User.find(receive_id)
     else
       @messages = Message.all
     end
@@ -31,21 +43,25 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
+    @message.save
+    @receive_user = User.find(@message.receive_user_id)
+    @send_user = User.find(@message.send_user_id)
 
+    redirect_to action: "index", :send_id => @message.send_user_id, :receive_id => @message.receive_user_id
 
-    respond_to do |format|
-      if @message.save
-        #@send_user = User.find(@message.send_user_id)
-        #@receive_user = User.find(@message.receive_user_id)
-        format.html { redirect_to :action => "index" ,:flash => {:send_id => @message.send_user_id, :receive_id => @message.receive_user_id  } }
-        #format.json { render :index, status: :created, location: @message }
-        #format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        #format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+#    respond_to do |format|
+#        @send_user = User.find(@message.send_user_id)
+  #      @receive_user = User.find(@message.receive_user_id)
+  #      redirect_to action: "index"
+  #      #format.html　{render :action => "index" ,:flash => {:send_id => @message.send_user_id, :receive_id => @message.receive_user_id  } }
+  #      #format.json { render :index, status: :created, location: @message }
+  #      #format.html { redirect_to @message, notice: 'Message was successfully created.' }
+  #      #format.json { render :show, status: :created, location: @message }
+  #    else
+  #      format.html { render :new }
+  #      format.json { render json: @message.errors, status: :unprocessable_entity }
+  #    end
+  #  end
   end
 
   # PATCH/PUT /messages/1
