bea30d3aeda26a8036e5af59149a2c3667743669
zukako <zukako@GirlsDeadMonster.local>
2018-03-07 14:45:07 +0900

    ログアウトの実装

--- matching/app/controllers/sessions_controller.rb
+++ matching/app/controllers/sessions_controller.rb
@@ -20,5 +20,12 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
+    log_out
+    redirect_to root_url
   end
+
+  # def logout
+  #   reset_session
+  #   redirect_to '/'
+  # end
 end
--- matching/config/routes.rb
+++ matching/config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :users
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  delete 'logout'  => 'sessions#destroy' #deleteの場合はmethod: :deleteが必要
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
