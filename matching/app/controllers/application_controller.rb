23810335aed889f3f3b8cb4d7d35136f640bd9a0
zukako <sakamoto.ryoma0930@gmail.com>
2018-03-31 18:49:05 +0900

    ログイン判定

--- matching/app/controllers/application_controller.rb
+++ matching/app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+  def signed_in?
+    current_user
+  end
+  def sec_signout
+    redirect_to controller:'sessions',acion:'new'
+  end
+
 end
